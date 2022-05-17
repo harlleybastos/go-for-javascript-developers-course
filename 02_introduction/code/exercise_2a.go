d43d0c6573533215f0026abce28c5862c0d51329
Harlley <harlleybastos@hotmail.com>
2022-05-17 13:40:19 -0300

    first commit

---
+++ 02_introduction/code/exercise_2a.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hi! My name is %s. I have lived in %s for %d years. They say the weather is amazing, which is %t.", "Harlley", "Rio Claro", 24, true)
+}
---
+++ practice.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world!")
+	fmt.Print("Hello, world!\n")
+	fmt.Printf("Hello, world %s!", "again")
+
+}
